0175e4cad920cb2270597c6aa6346d897ae9ac98
trincaw <nikxtr@gmail.com>
2022-05-26 11:33:57 +0200

    general fix

--- query.sql
+++ query.sql
@@ -1,19 +1,3 @@
-// Mostrare i turni di lavoro di un dipendente
-
-SELECT Ora_Inizio, Ora_Fine FROM Lavora WHERE IDPersonale = 20
-
-// Mostrare i dipendenti che hanno turni registrati
-
-SELECT DISTINCT Nome, Cognome FROM Personale INNER JOIN Lavora ON  Personale.IDPersonale=Lavora.IDPersonale
-
-////////////////////////////////
-
-// Mostrare i le ultime 10 transazioni di un cliente ( o ultima settimana)
-
-SELECT COUNT(IDPostazione) FROM Transazione WHERE IDPostazione = 4 AND Tipo='Vincita'
-34 AR
-
-
  ________ .-./`) ,---.   .--.   ____      .---.    .-./`)  
 |        |\ .-.')|    \  |  | .'  __ `.   | ,_|    \ .-.') 
 |   .----'/ `-' \|  ,  \ |  |/   '  \  \,-./  )    / `-' \ 
@@ -25,55 +9,52 @@ SELECT COUNT(IDPostazione) FROM Transazione WHERE IDPostazione = 4 AND Tipo='Vin
 '---'      '---' '--'    '--' '.(_,_).'   `--------`'---'  
                                                            
 
--- 1 // MOSTRARE I GIOCHI CON UN PAYOUT SUPERIORE ALLA MEDIA 
+------- 1 // MOSTRARE I GIOCHI CON UN PAYOUT SUPERIORE ALLA mediaIA 
 
 SELECT g.Nome, AVG(tx.Importo) FROM Transazione tx 
-INNER JOIN Postazioni ps ON tx.IDPostazione = ps.Numero 
-INNER JOIN Giochi as g ON g.IDGioco = ps.IDGioco 
+INNER JOIN Postazioni AS ps ON tx.IDPostazione = ps.Numero 
+INNER JOIN Giochi AS g ON g.IDGioco = ps.IDGioco 
 WHERE tx.tipo='Vincita' 
 GROUP BY g.Nome 
 HAVING AVG(tx.Importo) > (SELECT AVG(tx.Importo) FROM Transazione tx WHERE tx.tipo='Vincita')
 
--- 2 // Mostrare statistiche tavolo vincite sconfitte
+------- 2 // Mostrare statistiche tavolo vincite sconfitte
 
 -- // CON VIEW
 DROP VIEW IF EXISTS vincite;
 CREATE VIEW vincite AS
-SELECT Nome, COUNT(IDTx) as Vincite FROM Postazioni LEFT JOIN Transazione ON Numero=IDPostazione 
+SELECT Nome, COUNT(IDTx) AS Vincite FROM Postazioni LEFT JOIN Transazione ON Numero=IDPostazione 
 WHERE Tipo='Vincita' GROUP BY Nome,Tipo;
  
 DROP VIEW IF EXISTS perdite;
 CREATE VIEW perdite AS
-SELECT Nome, COUNT(IDTx) as Perdite FROM Postazioni LEFT JOIN Transazione ON Numero=IDPostazione 
+SELECT Nome, COUNT(IDTx) AS Perdite FROM Postazioni LEFT JOIN Transazione ON Numero=IDPostazione 
 WHERE Tipo='Perdita' GROUP BY Nome,Tipo ;
 
 SELECT v.Nome,Vincite,Perdite FROM vincite v INNER JOIN perdite p ON v.Nome = p.Nome  
 
 -- // SENZA VIEW
 
-SELECT vin.Nome,Vincite,vin.med,Perdite,per.med FROM 
-(SELECT Nome, COUNT(IDTx) as Vincite, AVG(Importo) as med FROM Postazioni LEFT JOIN Transazione ON Numero=IDPostazione 
-WHERE Tipo='Vincita' GROUP BY Nome,Tipo) as vin 
+SELECT vin.Nome,Vincite,vin.media,Perdite,per.media FROM 
+(SELECT Nome, COUNT(IDTx) AS Vincite, AVG(Importo) AS media FROM Postazioni LEFT JOIN Transazione ON Numero=IDPostazione 
+WHERE Tipo='Vincita' GROUP BY Nome,Tipo) AS vin 
 INNER JOIN 
-(SELECT Nome, COUNT(IDTx) as Perdite, AVG(Importo) as med FROM Postazioni LEFT JOIN Transazione ON Numero=IDPostazione 
-WHERE Tipo='Perdita' GROUP BY Nome,Tipo) as per ON vin.Nome = per.Nome WHERE vin.med > per.med
+(SELECT Nome, COUNT(IDTx) AS Perdite, AVG(Importo) AS media FROM Postazioni LEFT JOIN Transazione ON Numero=IDPostazione 
+WHERE Tipo='Perdita' GROUP BY Nome,Tipo) AS per ON vin.Nome = per.Nome WHERE vin.media > per.media
 
--- 3 // Mostrare il cliente che ha vinto la maggior somma di denaro alla roulette
+------- 3 // Mostrare il cliente che ha vinto la maggior somma di denaro alla roulette
 
 SELECT DISTINCT Nome, Cognome, Importo FROM Clienti 
 INNER JOIN Transazione ON Clienti.IDCliente=Transazione.IDCliente 
 WHERE Importo = (SELECT MAX(Importo) FROM Transazione 
 WHERE Tipo='Vincita' AND IDPostazione IN (SELECT Numero FROM Postazioni WHERE IDGioco IN (SELECT IDGioco FROM Giochi WHERE Tipo ='Roulette')))
 
--- 4 // Mostrare il vincitore di un torneo e 
+------- 4 // Mostrare il vincitore di un torneo e 
 
 SELECT c.Nome,c.Cognome, disp.SaldoMano FROM Clienti c 
-INNER JOIN Disputano disp ON disp.IDCliente = c.IDCliente 
-INNER JOIN Match m ON disp.IDMatch = m.IDMatch 
+INNER JOIN Disputano AS disp ON disp.IDCliente = c.IDCliente 
+INNER JOIN Match AS m ON disp.IDMatch = m.IDMatch 
 WHERE m.IsFinal = true AND m.IDTorneo = 1 AND disp.SaldoMano != 0 
-
-
-
 
 .-./`) ,---.   .--. ______         .-''-.   _____     __   
 \ .-.')|    \  |  ||    _ `''.   .'_ _   \  \   _\   /  /  
